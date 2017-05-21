d1fa5cbad691aa691747adb123276c46805d6d95
Masashi Kayahara <sylphs.mb@gmail.com>
2017-05-22 00:32:47 +0900

    Add material-ui

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Layout from './pages/Layout'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app );
--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -4,13 +4,20 @@
  */
 import React from "react";
 import Header from "../components/Header";
+import getMuiTheme from "material-ui/styles/getMuiTheme"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import RaisedButton from "material-ui/RaisedButton"
+import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme"
 
 export default class Layout extends React.Component {
     render() {
         return (
-            <div>
-                <Header />
-            </div>
-        );
+            <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+                <div>
+                    <Header />
+                    <RaisedButton label="Default" />
+                </div>
+            </MuiThemeProvider>
+        )
     }
 }
